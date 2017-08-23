d3576d8db917359c06849ffd5b7addf9294d5122
gh0stsh0t <rjperias0213@gmail.com>
2017-08-23 15:25:25 +0800

    The unicorn lives on

---
+++ unicorn.c
@@ -0,0 +1,48 @@
+/*
+
+Skeletons are scary, so here's a fanciful unicorn to wish you good luck.
+
+    \
+     \ji
+     /.((( 
+    (,/"(((__,--.
+        \  ) _( /{ 
+        !|| " :||      
+        !||   :|| 
+        '''   '''
+
+*/
+
+//***********************************
+// The helpful unicorn says:
+// It's scanf("%f", &somenumber);
+// NOT scanf("%.2f", &somenumber);
+//***********************************
+
+
+// First, get the number of items bought n
+
+// Next, loop through n times
+    // Get the price of an item, add it to the total
+
+// Finally, get the amount tendered t
+
+// Now, how do you get the change or balance? Hmmm...
+// Oh, right! change = tender - total
+
+// if change is positive or zero, then print change
+// otherwise, print it as a balance
+
+//***********************************
+// The helpful unicorn says:
+//    if(change >= 0) {
+//          print change
+//    } else {
+//          .....
+// }
+//***********************************
+
+//***********************************
+// The helpful unicorn says:
+//    A negative number times a negative number is positive.
+//***********************************
